478c71ac786fd972549d35d9b669526cdbfc7b8d
pancudaniel7 <pancudaniel7@gmail.com>
2021-12-30 17:12:43 +0200

    Fixed PhantomJS problem

--- src/scraping_common_bot/scraping/loader/loader.py
+++ src/scraping_common_bot/scraping/loader/loader.py
@@ -1,7 +1,7 @@
 import time
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.wb import WebDriver
+from selenium.webdriver.web import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
